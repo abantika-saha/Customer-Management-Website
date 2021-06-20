7b33806294ce89c0b91860a6b7ddc90ab2550cbb
Abantika Saha <65250567+abantika-saha@users.noreply.github.com>
2021-06-20 19:16:27 +0530

    Add files via upload

--- accounts/models.py
+++ accounts/models.py
@@ -8,15 +8,12 @@ class Customer(models.Model):
 	email = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-
 	def __str__(self):
 		return self.name
 
 
 class Tag(models.Model):
 	name = models.CharField(max_length=200, null=True)
-
-
 
 	def __str__(self):
 		return self.name
@@ -32,12 +29,11 @@ class Product(models.Model):
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
 	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	tags = models.ManyToManyField(Tag) 
+	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.name
 
-	
 class Order(models.Model):
 	STATUS = (
 			('Pending', 'Pending'),
@@ -45,7 +41,11 @@ class Order(models.Model):
 			('Delivered', 'Delivered'),
 			)
 
-	customer = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
-	product = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	note = models.CharField(max_length=1000, null=True)
+
+	def __str__(self):
+		return self.product.name
